ee7548412ed046c76b6786723419ddb35b560d3d
everton-freitas <evertonfreitas016@hotmail.com>
2024-11-26 14:10:24 -0300

    update files

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 function aparecerMenu() {
-    let menu_oculto = document.querySelector('div#menu-oculto')
-    if (menu_oculto.style.display === 'block') {
-        menu_oculto.style.display = 'none'
-    } else {
-        menu_oculto.style.display = 'block'
-    }
+    const menu = document.getElementById('menu-oculto');
+    menu.classList.toggle('show');
+    header.classList.toggle('push'); // Empurra ou retorna o header
 }
